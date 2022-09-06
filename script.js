3dfdbcc537e48205583c59263cf3c70860f389be
unknown <C:\Users\user\AppData\Roaming\The Bat!\Mail>
2022-09-06 16:59:34 +1000

    added event listeners

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var container = document.getElementById("grid-container");
 
 var squaresHorizontal = 16;
 var squaresVertical = 16;
+var trigger = false;
 
 for (var i = 0; i < squaresHorizontal * squaresVertical; i++) {
     var square = document.createElement("div");
@@ -9,3 +10,20 @@ for (var i = 0; i < squaresHorizontal * squaresVertical; i++) {
     container.appendChild(square);
 }
 
+var squares = document.querySelectorAll(".square");
+
+squares.forEach((item) => {
+    item.addEventListener('mouseenter', () => {
+        if (trigger === true) {
+            item.style.backgroundColor = "red";
+        }
+    })
+})
+
+document.addEventListener('mousedown', function(){
+    trigger = true;
+});
+
+document.addEventListener('mouseup', function(){
+    trigger = false;
+});
