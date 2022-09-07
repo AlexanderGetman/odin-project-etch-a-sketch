c93e6cca1e6e492fd4af1c36052166d858ec39e3
unknown <C:\Users\user\AppData\Roaming\The Bat!\Mail>
2022-09-07 16:33:44 +1000

    Added random colors change + change some events

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ var container = document.getElementById("grid-container");
 var squaresHorizontal = 16;
 var squaresVertical = 16;
 var trigger = false;
+var color = "#ff0000";
+
+document.querySelector("#color-select").onchange = e => {
+    color = e.target.value;
+ }
 
 for (var i = 0; i < squaresHorizontal * squaresVertical; i++) {
     var square = document.createElement("div");
@@ -15,15 +20,32 @@ var squares = document.querySelectorAll(".square");
 squares.forEach((item) => {
     item.addEventListener('mouseenter', () => {
         if (trigger === true) {
-            item.style.backgroundColor = "red";
+            item.style.backgroundColor = color;
         }
     })
+
+    item.addEventListener('mousedown', () => {
+        item.style.backgroundColor = color;        
+    })
 })
+
+document.addEventListener('mouseup', function(){
+    trigger = false;
+});
 
 document.addEventListener('mousedown', function(){
     trigger = true;
 });
 
-document.addEventListener('mouseup', function(){
-    trigger = false;
+function change() {
+    color = "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+
+var checkbox = document.getElementById('random-color-checkbox');
+
+checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+        change();
+        setInterval(change, 100);
+    }
 });
