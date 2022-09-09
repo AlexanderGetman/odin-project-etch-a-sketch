80f2a59b049ad186c78920d446c922fbfb9e2e8b
unknown <C:\Users\user\AppData\Roaming\The Bat!\Mail>
2022-09-09 15:36:22 +1000

    Added rows number slider

--- script.js
+++ script.js
@@ -1,32 +1,63 @@
 var container = document.getElementById("grid-container");
 
-var squaresHorizontal = 16;
-var squaresVertical = 16;
+var slider = document.querySelector("#slider");
 var trigger = false;
 var color = "#ff0000";
-
-document.querySelector("#color-select").onchange = e => {
-    color = e.target.value;
-}
+var numberOfRows = 16;
+var squaresHorizontal = numberOfRows;
+var squaresVertical = numberOfRows;
+var numberOfRowsText = document.getElementById("number-of-rows");
+numberOfRowsText.textContent = 16;
 
 for (var i = 0; i < squaresHorizontal * squaresVertical; i++) {
     var square = document.createElement("div");
     square.className = "square";
     container.appendChild(square);
+    square.style.width = (400/numberOfRows) + 'px';
+    square.style.height = (400/numberOfRows) + 'px';
 }
 
 var squares = document.querySelectorAll(".square");
 
-squares.forEach((item) => {
-    item.addEventListener('mouseenter', () => {
-        if (trigger === true) {
+function paint() {
+    squares.forEach((item) => {
+        item.addEventListener('mouseenter', () => {
+            if (trigger === true) {
+                item.style.backgroundColor = color;
+            }
+        })
+    
+        item.addEventListener('mousedown', () => {
             item.style.backgroundColor = color;
-        }
+        })
     })
+}
 
-    item.addEventListener('mousedown', () => {
-        item.style.backgroundColor = color;        
-    })
+paint()
+
+document.querySelector("#color-select").onchange = e => {
+    color = e.target.value;
+}
+
+slider.addEventListener('change', () => {
+    numberOfRows = slider.value;
+    squaresHorizontal = numberOfRows;
+    squaresVertical = numberOfRows;
+    container.textContent = '';
+
+    
+    for (var i = 0; i < squaresHorizontal * squaresVertical; i++) {
+        var square = document.createElement("div");
+        square.className = "square";
+        container.appendChild(square);
+        square.style.width = (400/numberOfRows) + 'px';
+        square.style.height = (400/numberOfRows) + 'px';
+    }
+    squares = document.querySelectorAll(".square");
+
+    paint()
+
+    document.getElementById("number-of-rows").textContent = numberOfRows;
 })
 
 document.addEventListener('mouseup', function(){
